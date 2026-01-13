bc5e2f20d978bea770bb4ea214068b1ada378756
Victor López <victorlopez_20@outlook.com>
2026-01-13 16:05:23 -0600

    update: Set up the Product class

---
+++ model/Product.php
@@ -0,0 +1,10 @@
+<?php
+    require_once './db/Connection.php';
+
+    class Product extends Connection
+    {
+        // Atributo para la conexion
+        private $connection = NULL;
+        
+        
+    }
